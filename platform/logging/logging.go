5d98eeaed461b9f9c3c8ef6235d957f6d28d01b8
yuuwatanabe <panyoriokome@gmail.com>
2022-03-13 15:17:11 +0900

    ch1: logging

---
+++ platform/logging/logging.go
@@ -0,0 +1,31 @@
+package logging
+
+import "fmt"
+
+type LogLevel int
+
+const (
+	Trace LogLevel = iota
+	Debug
+	Information
+	Warning
+	Fatal
+	None
+)
+
+type Logger interface {
+	Trace(string)
+	Tracef(string, ...interface{})
+	Debug(string)
+	Debugf(string, ...interface{})
+	Info(string)
+	Infof(string, ...interface{})
+	Warn(string)
+	Warnf(string, ...interface{})
+	Panic(string)
+	Panicf(string, ...interface{})
+}
+
+func main() {
+	fmt.Println(Trace)
+}
